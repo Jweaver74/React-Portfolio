2c0ca5a6be05127cd001a8e66a7c2cde96c7ec79
Jason Weaver <119889739+Jweaver74@users.noreply.github.com>
2023-07-26 13:15:36 -0500

    update

--- src/projects.js
+++ src/projects.js
@@ -5,6 +5,7 @@ export const projects =[
         "Homeward Bound is a full stack application that allows users to search for adoptable pets in their area. Users can search by zip code, animal type, and breed. Users can also save pets to their favorites list and view them later. This application uses the Petfinder API to retrieve pet data.",
         imageLink:
         'https://raw.githubusercontent.com/Jweaver74/homeward-bound/f31e6f84be8fa51b9ca22b944c73e2a0fb799a7e/public/images/Homepage.png',
+        livelink:"https://warm-fortress-51039.herokuapp.com/",
         githubLink: 
         'https://github.com/Jweaver74/homeward-bound'
     },
