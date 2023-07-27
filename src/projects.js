0c5c9a29a8d97e7c4b9b7b2d82a72f86f1c00a26
Jason Weaver <119889739+Jweaver74@users.noreply.github.com>
2023-07-27 12:43:31 -0500

    added live links

--- src/projects.js
+++ src/projects.js
@@ -1,13 +1,13 @@
 export const projects =[
     {
-        projectTitle: 'Homeward Bound',
+        projectTitle: "Homeward Bound",
         description:
         "Homeward Bound is a full stack application that allows users to search for adoptable pets in their area. Users can search by zip code, animal type, and breed. Users can also save pets to their favorites list and view them later. This application uses the Petfinder API to retrieve pet data.",
         imageLink:
-        'https://raw.githubusercontent.com/Jweaver74/homeward-bound/f31e6f84be8fa51b9ca22b944c73e2a0fb799a7e/public/images/Homepage.png',
+        "https://raw.githubusercontent.com/Jweaver74/homeward-bound/f31e6f84be8fa51b9ca22b944c73e2a0fb799a7e/public/images/Homepage.png",
         livelink:"https://warm-fortress-51039.herokuapp.com/",
         githubLink: 
-        'https://github.com/Jweaver74/homeward-bound'
+        "https://github.com/Jweaver74/homeward-bound"
     },
     {
         projectTitle: 'Dinner and a Movie',
@@ -34,6 +34,7 @@ export const projects =[
         "This application allows users to create a custom SVG logo. Users can change the color of the logo and download the logo as a PNG file.",
         imageLink:
         "https://github.com/Jweaver74/Creative-Logo-Creator/blob/main/examples/orange%20triangle.png?raw=true",
+        livelink:"https://jweaver74.github.io/Creative-Logo-Creator/",
         githubLink:
         "https://github.com/Jweaver74/Creative-Logo-Creator",
     },
@@ -43,6 +44,7 @@ export const projects =[
         "This application allows users to save events for each hour of the day. This application runs in the browser and features dynamically updated HTML and CSS powered by jQuery.",
         imageLink:
         "https://github.com/Jweaver74/Hetherington-Apiaries/blob/main/images/updated%20screen%20shot.PNG?raw=true",
+        livelink:"https://jweaver74.github.io/Hetherington-Apiaries/",
         githubLink:
         "https://github.com/Jweaver74/Hetherington-Apiaries",
     },
@@ -52,6 +54,7 @@ export const projects =[
         "This application allows users to search for the current weather and 5 day forecast for any city. The application uses the OpenWeather API to retrieve weather data.",
         imageLink:
         'https://raw.githubusercontent.com/Jweaver74/Your-Local-Weather/main/assets/images/Screenshot%202023-04-04%20100817.png',
+        livelink:"https://jweaver74.github.io/Your-Local-Weather/",
         githubLink:
         'https://github.com/Jweaver74/Your-Local-Weather'
     },
