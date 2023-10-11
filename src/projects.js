6fba5f895b0165833610f0e880c82bc785c99f00
Jason Weaver <119889739+Jweaver74@users.noreply.github.com>
2023-10-11 12:49:59 -0500

    updated projects

--- src/projects.js
+++ src/projects.js
@@ -14,7 +14,7 @@ export const projects = [
       "Dinner and a Movie is a full stack application that allows users to search for movies and places to eat. Users can search for movies by title and movies by genre. Users can also save movies and recipes to their favorites list and view them later.",
     imageLink:
       "https://github.com/Jweaver74/Dinner-and-a-Movie/blob/main/assets/images/start%20screen.png?raw=true",
-    livelink: "https://dinner-and-a-movie-74.herokuapp.com/",
+    livelink: "https://mvincent15.github.io/Dinner-and-a-Movie/",
     github: "https://github.com/Jweaver74/Dinner-and-a-Movie",
   },
   {
@@ -23,17 +23,17 @@ export const projects = [
       "This application allows users to create, save, and delete notes. This application uses an Express.js back end and saves and retrieves note data from a JSON file.",
     imageLink:
       "https://github.com/Jweaver74/Taking-Notes/blob/main/public/assets/images/home%20screen.png?raw=true",
-    livelink: "https://taking-notes-74.herokuapp.com/",
+    livelink: "https://glacial-gorge-53622.herokuapp.com/",
     github: "https://github.com/Jweaver74/Taking-Notes",
   },
   {
-    projectTitle: "SVG logo creator",
+    projectTitle: "Coding Quiz Challenge",
     description:
-      "This application allows users to create a custom SVG logo. Users can change the color of the logo and download the logo as a PNG file.",
+      "This application is a timed coding quiz with multiple-choice questions. This app runs in the browser and features dynamically updated HTML and CSS powered by JavaScript code.",
     imageLink:
-      "https://github.com/Jweaver74/Creative-Logo-Creator/blob/main/examples/orange%20triangle.png?raw=true",
-    livelink: "https://jweaver74.github.io/Creative-Logo-Creator/",
-    github: "https://github.com/Jweaver74/Creative-Logo-Creator",
+      "https://raw.githubusercontent.com/Jweaver74/Classmate-Code-Quiz/main/assets/images/Quiz%20frist%20page.png",
+    livelink: "https://jweaver74.github.io/Classmate-Code-Quiz/",
+    github: "https://github.com/Jweaver74/Classmate-Code-Quiz",
   },
   {
     projectTitle: "Day Scheduler",
